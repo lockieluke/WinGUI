3a1ecc72c18954dfe2509bda5536ace845888211
lockieluke <sherlockluk@outlook.com>
2020-09-04 19:29:48 +0800

    Window Class

--- Window/Window.cpp
+++ Window/Window.cpp
@@ -4,6 +4,74 @@
 
 #include "Window.h"
 
-Window::Window(const char* Title) {
-    this->Title = (char *)Title;
+Window::Window(HINSTANCE hinstance, int nCmdShow) {
+    const char g_szClassName[] = "myWindowClass";
+
+    WNDCLASSEX wc;
+    wc.cbSize        = sizeof(WNDCLASSEX);
+    wc.style         = 0;
+    wc.lpfnWndProc   = WndProc;
+    wc.cbClsExtra    = 0;
+    wc.cbWndExtra    = 0;
+    wc.hInstance     = hinstance;
+    wc.hIcon         = LoadIcon(NULL, IDI_APPLICATION);
+    wc.hCursor       = LoadCursor(NULL, IDC_ARROW);
+    wc.hbrBackground = (HBRUSH)(COLOR_WINDOW+1);
+    wc.lpszMenuName  = NULL;
+    wc.lpszClassName = g_szClassName;
+    wc.hIconSm       = LoadIcon(NULL, IDI_APPLICATION);
+
+    if(!RegisterClassEx(&wc))
+    {
+        MessageBox(NULL, "Window Registration Failed!", "Error!",
+                   MB_ICONEXCLAMATION | MB_OK);
+    }
+
+    hwnd = CreateWindowEx(
+            WS_EX_CLIENTEDGE,
+            g_szClassName,
+            "",
+            WS_OVERLAPPEDWINDOW,
+            CW_USEDEFAULT, CW_USEDEFAULT, 1280, 720,
+            NULL, NULL, hinstance, NULL);
+
+    if(hwnd == NULL)
+    {
+        MessageBox(NULL, "Window Creation Failed!", "Error!",
+                   MB_ICONEXCLAMATION | MB_OK);
+    }
+
+    ShowWindow(hwnd, nCmdShow);
+    UpdateWindow(hwnd);
+
+    while(GetMessage(&msg, NULL, 0, 0) > 0)
+    {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+}
+
+void Window::SetTitle(const char *title) {
+    MessageBox(hwnd, "", "", MB_OK | MB_ICONINFORMATION);
+}
+
+WPARAM Window::ReturnMsg() {
+    return (msg.wParam);
+}
+
+LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
+    switch (msg) {
+        case WM_CLOSE:
+            DestroyWindow(hwnd);
+            break;
+
+        case WM_DESTROY:
+            PostQuitMessage(0);
+            break;
+
+        default:
+            return DefWindowProc(hwnd, msg, wParam, lParam);
+    }
+
+    return 0;
 }
--- Window/Window.h
+++ Window/Window.h
@@ -2,16 +2,28 @@
 // Created by Sherlock Luk on 9/4/2020.
 //
 
-#ifndef WINGUI_API_WINDOW_H
-#define WINGUI_API_WINDOW_H
+#pragma once
 
+#include <windows.h>
+#include <commctrl.h>
 #include <iostream>
 
+#ifndef WINGUI_API_WINDOW_H
+
+#define WINGUI_API_WINDOW_H
+
 class Window {
+private:
+    MSG msg = {};
+    HWND hwnd;
 public:
-    Window(const char* Title = "");
-    char * Title;
+    HWND hwnd;
+    Window(HINSTANCE hinstance, int nCmdShow);
+    void SetTitle(const char* title);
+    WPARAM ReturnMsg();
 };
+
+LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam);
 
 
 #endif //WINGUI_API_WINDOW_H
